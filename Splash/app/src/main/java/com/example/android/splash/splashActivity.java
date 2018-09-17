85724d7f446bc14dd765055b787f61508e2d74f9
Moahamed amine <beng.med.amine@gmail.com>
2018-09-17 20:08:55 +0200

    3th Project

--- Splash/app/src/main/java/com/example/android/splash/splashActivity.java
+++ Splash/app/src/main/java/com/example/android/splash/splashActivity.java
@@ -10,23 +10,27 @@ public class splashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_splash);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE); // هذا السطر لنزع عنوان التطبيق  من الأكتيفيتي
+		
+        setContentView(R.layout.activity_splash); // جعل سبلاش لايوت كواجهة لهذه الأكتيفيتي
 
-        Thread s = new Thread(){
+            Thread s = new Thread(){ // انشاء كائن من نوع ثريد  الذي يسمح لنا بتشغيله بشكل متزامن مع التطبيق
+
             @Override
-            public void run() {
-                try {
-                    sleep(5*1000);
-                    Intent i = new Intent(splashActivity.this,MainActivity.class);
-                    startActivity(i);
-                    finish();
+            public void run() { // الدالة التي تستدع عن طريق ستارت
+
+                                try {
+                    sleep(3*1000); // وقت الانتظار
+                    Intent i = new Intent(splashActivity.this,MainActivity.class); // انشاء انتنت لتوجيه من أكتيفيتي الحالية الى الأكتيفيتي الموالية
+                    startActivity(i); 
+                    finish(); // الأكتيفيتي الحالية
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         };
 
-    s.start();
+	    s.start();// استدعاء الدالة  run 
+
     }
 }
